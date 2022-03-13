fab65a00fbcc3d555fb21a2de137f3df160777aa
fluentmin <fluentmin@gmail.com>
2022-03-13 22:05:37 +0900

    indexClock
    Adding clock function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-
+const clock = document.querySelector(".status-bar__column:nth-child(2) span")
 
 function onLoginSubmit(event) {
     const username = loginInput.value;
@@ -9,4 +9,14 @@ function onLoginSubmit(event) {
 
 loginForm.addEventListener("submit", onLoginSubmit);
 
+function getClock() {
+    const date = new Date();
 
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+
+    clock.innerText = (`${hours}:${minutes}`);
+}
+
+getClock();
+setInterval(getClock, 1000);
